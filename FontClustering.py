6a79143377739929261bb55a281d8a23cebf2275
akatarey <arkinkatarey@gmail.com>
2023-10-21 16:55:41 -0700

    Imported necessary libraries

--- FontClustering.py
+++ FontClustering.py
@@ -1,1 +1,18 @@
 from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from random import randint
+import pandas as pd
+import pickle
+
+
