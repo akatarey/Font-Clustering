b2675b572996f2327a480e954987d52a25020911
akatarey <arkinkatarey@gmail.com>
2023-11-15 16:56:33 -0800

    Initial PIL image conversion method

--- FontClustering.py
+++ FontClustering.py
@@ -14,9 +14,26 @@ import matplotlib.pyplot as plt
 from random import randint
 import pandas as pd
 import pickle
-
+from PIL import Image, ImageFont, ImageDraw
 #Convert ttf into image files, then plug in
 
-data_folder = Path("C:/Users/arkin/Desktop/WorkGoals.txt")
-f = open(data_folder, "r")
-print(f.read())
+font_path = Path("C:/Users/arkin/Desktop/Fonts/Roboto-Regular.ttf")
+font_name = "Roboto-Regular.ttf"
+out_path = Path("C:/Users/arkin/Desktop/Fonts/")
+
+font_size = 16 #px
+font_color = "#000000" #HEX for black
+
+font = ImageFont.truetype(str(font_path), font_size)
+
+desired_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+
+for character in desired_characters:
+    width, height = font.getsize(character)
+    img = Image.new("RGBA", (width, height))
+    draw = ImageDraw.Draw(img)
+    draw.text((-2, 0), str(character), font=font, fill=font_color)
+    try:
+        img.save(str(out_path) + str(ord(character)) + ".png")
+    except:
+        print(f"[-] Couldn't save:\t{character}")
