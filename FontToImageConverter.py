eda6c6025488a268ff63b9ff5eab28ca03f45b94
akatarey <arkinkatarey@gmail.com>
2023-11-18 00:27:36 -0800

    Img has right dimensions and color

--- FontToImageConverter.py
+++ FontToImageConverter.py
@@ -15,11 +15,10 @@ font_color = "#000000" #HEX for black
 
 font = ImageFont.truetype(str(font_path), font_size)
 
-characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
-init_char = "Q"
+characters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9 0"
 
-width, height = font.getsize(init_char)
-img = Image.new("RGBA", (width * 100, height * 100))
+width, height = font.getsize("Q")
+img = Image.new("RGBA", (width * 100, height * 2), color="white")
 draw = ImageDraw.Draw(img)
 draw.text((0, 0), characters, font=font, fill=font_color)
 try:
