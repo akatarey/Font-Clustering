8f7704c2d1161b7a358e02058ac3b9bf5404db4f
akatarey <arkinkatarey@gmail.com>
2023-11-18 16:03:14 -0800

    Aggregated all imgs

--- FontClustering.py
+++ FontClustering.py
@@ -9,12 +9,21 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
 from pathlib import Path
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from random import randint
 import pandas as pd
 import pickle
-from PIL import Image, ImageFont, ImageDraw
 #Convert ttf into image files, then plug in
 
-print("Start work on image processing")
+path = Path("C:/Users/arkin/Desktop/Fonts")
+os.chdir(path)
+
+font_imgs = []
+with os.scandir(path) as files:
+    for file in files:
+        if file.name.endswith(".png"):
+            font_imgs.append(file.name)
+
+print(font_imgs)
